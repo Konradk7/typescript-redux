f93ae98c405fcad0473f68ad8f0d704a9f4c29d9
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-08-05 09:12:51 +0200

    Add reducer JS

--- src/store/redux.ts
+++ src/store/redux.ts
@@ -1,4 +1,6 @@
 import React from 'react';
+import any = jasmine.any;
+import {createStore} from "redux";
 
 // 1. State
 const defaultState = {
@@ -45,6 +47,34 @@ const removeAllTodos = () => ({
 
 // 3. Reducer
 
-const reducer = (state = defaultState, reducer: any) => {
-    
-}
+const reducer = (state = defaultState, action: any) => {
+    switch (action.type) {
+        case TODOS_ACTIONS_TYPES.ADD:
+            return {...state, todos: [...state.todos, action.payload]};
+        case TODOS_ACTIONS_TYPES.DELETE:
+            const filteredTodos = state.todos.filter(
+                (id) => id !== action.payload)
+            return {...state, todos: filteredTodos}
+        case TODOS_ACTIONS_TYPES.UPDATE:
+            const todos = state.todos.map(todo => {
+                if (todo.id === action.payload.id) {
+                    return {
+                        ...todo,
+                        message: action.payload.message
+                    };
+                }
+                return todo;
+            })
+            return { ...state, todos };
+        case TODOS_ACTIONS_TYPES.REMOVE_ALL:
+            return {...state, todos: []};
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+// 4. Dispatch
+
+
